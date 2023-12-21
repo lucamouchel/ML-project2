21ca3e5f664ae7d0201fa507ed384be0e40e176c
colin-a-smyth <145272391+colin-a-smyth@users.noreply.github.com>
2023-12-21 15:45:06 +0100

    Update load_data_testing.py - comments

--- project/src/testingScripts/load_data_testing.py
+++ project/src/testingScripts/load_data_testing.py
@@ -11,25 +11,38 @@ class DatasetLoader():
         self.tokenizer = tokenizer
     
     def load_data(self):
+        """
+        Loads in data from data/train_pos.txt and data/train_neg.txt
+        """
+        # Loading in positive and negative samples
         pos = pd.read_csv('data/train_pos.txt', header=None, delimiter='\t').sample(100, random_state=4)
         pos['label'] = 1
         neg = pd.read_csv('data/train_neg.txt', header=None, delimiter='\t').sample(100, random_state=4)
         neg['label'] = 0
+        
+        train_test_split = 0.8 # 80% of data to training, 20% to testing
 
-        train_test_split = 0.8
-
+        # Concatenation of positive and negative samples
         full_df = pd.concat([pos, neg]).sample(frac=1, random_state=42)
+        
         train_df = full_df[:int(len(full_df)*train_test_split)]
+        # Subsection of training to validate DURING training
         dev_df = train_df.sample(frac=0.2, random_state=42).rename(columns={0: 'tweet'})
+        # Removing dev_df lines from train_df
         train_df = train_df.drop(dev_df.index).rename(columns={0: 'tweet'})
+
+        # Creating test df from remaining lines
         test_df = full_df[int(len(full_df)*train_test_split):].rename(columns={0: 'tweet'})
-        # test_df = pd.read_csv('data/test_data.txt', delimiter='\t', header=None).rename(columns={0: 'tweet'})
+        # Validation is copy of test_df with the labels
         validation_df = test_df.copy()
         test_df['label'] = -2
         test_df = test_df
         return train_df, dev_df, test_df, validation_df
 
     def load_dataset(self, split="train"):
+        """
+        Calls in dataset depending on split that was asked for
+        """
         train, dev, test, validation = self.load_data()
         if split == 'train':
             df = train
